878c43474b6610d073c107c44adf02c35d5901ec
madutech7 <madutech7@gmail.com>
2026-08-08 14:50:33 +0000

    Add custom iOS AppIcon and fix SampleData compilation

--- MonJardin/Models/SampleData.swift
+++ MonJardin/Models/SampleData.swift
@@ -4,6 +4,6 @@ import SwiftData
 /// Utilitaire de données - Démarrage propre avec base locale vide
 public struct SampleData {
     public static func populate(context: ModelContext) {
-        // Aucune donnée factice insérée. L'utilisateur commence avec un jardin vierge.
+        // Base de données locale vierge au démarrage
     }
 }
